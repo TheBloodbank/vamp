7b6cb0e7f756eb2985267f8869ec538980303b3a
Sam Hart <hartsn@gmail.com>
2016-01-09 17:38:26 -0500

    config a borg'd class would be better

--- vamp/config.py
+++ vamp/config.py
@@ -6,22 +6,28 @@ except ImportError:
 from vamp.in_a_world import get_config_file, get_install_path, \
         get_bin_path
 
-def load_config():
-    config = configparser.ConfigParser()
-    config_file = get_config_file()
-    config.read(config_file)
-    save_config = False
+class Config:
+    __borg_state = {}
 
-    # Sensible defaults
-    if 'paths' not in config:
-        config['paths'] = {
+    def __init__(self):
+        self.__dict__ = self.__borg_state
+
+    def load_config(self):
+        config = configparser.ConfigParser()
+        config_file = get_config_file()
+        config.read(config_file)
+        save_config = False
+
+        # Sensible defaults
+        if 'paths' not in config:
+            config['paths'] = {
                 'install' : get_install_path(),
                 'bin' : get_bin_path()
                 }
-        save_config = True
+            save_config = True
 
-    if save_config:
-        with open(config_file, 'w') as cf:
-            config.write(cf)
+        if save_config:
+            with open(config_file, 'w') as cf:
+                config.write(cf)
 
-    return config
+        return config
