0e723a1e8c0d6851fb430fda780a773432cc88fc
Sam Hart <hartsn@gmail.com>
2016-01-08 21:15:43 -0500

    flesh out config

--- vamp/config.py
+++ vamp/config.py
@@ -1,10 +1,25 @@
-from vamp.in_a_world import get_config_file
-
 try:
-    import ConfigParser at configparser
-except ImportError:
     import configparser
+except ImportError:
+    import ConfigParser as configparser
+
+from vamp.in_a_world import get_config_file, get_install_path, \
+        get_bin_path
 
 def load_config():
+    config = configparser.ConfigParser()
     config_file = get_config_file()
+    config.read(config_file)
+    save_config = False
 
+    # Sensible defaults
+    if 'paths' not in config:
+        config['paths'] = {
+                'install' : get_install_path(),
+                'bin' : get_bin_path()
+                }
+        save_config = True
+
+    if save_config:
+        with open(config_file, 'w') as cf:
+            config.write(cf)
--- vamp/in_a_world.py
+++ vamp/in_a_world.py
@@ -1,5 +1,14 @@
+"""Utility module for a world gone mad..."""
+
+
 import os
 from os.path import expanduser
+
+def ensure_path(path):
+    """Ensures a path exists"""
+    if not os.path.exists(path):
+        # Possible race condition, but fuck it
+        os.makedirs(path)
 
 def get_config_file():
     """Returns the system-specific config file. Will be created if it doesn't
@@ -13,3 +22,35 @@ def get_config_file():
         config_home = '{0}/.config'.format(expanduser("~"))
 
     my_config = "{0}/vamp".format(config_home)
+
+    ensure_path(my_config)
+
+    return "{0}/config.ini".format(my_config)
+
+def get_install_path():
+    """Returns the system-specific default path for installation. Will be
+    created if it doesn't exist"""
+    if 'HOME' in os.environ:
+        base_path = expanduser(os.environ['HOME'])
+    else:
+        base_path = expanduser("~")
+
+    install_path = "{0}/vamp/install".format(base_path)
+
+    ensure_path(install_path)
+
+    return install_path
+
+def get_bin_path():
+    """Returns the system-specific default bin directory. Will be created if it
+    doesn't exist"""
+    if 'HOME' in os.environ:
+        base_path = expanduser(os.environ['HOME'])
+    else:
+        base_path = expanduser("~")
+
+    bin_path = "{0}/vamp/bin".format(base_path)
+
+    ensure_path(bin_path)
+
+    return bin_path
