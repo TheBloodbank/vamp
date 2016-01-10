5fede0e8c0a899f2035ff277803bc9f02946065e
Sam Hart <hartsn@gmail.com>
2016-01-09 20:59:19 -0500

    add a help subsystem stub

--- vamp/run.py
+++ vamp/run.py
@@ -10,26 +10,55 @@ from vamp.in_a_world import get_max_lines, get_max_columns
 MAX_PAGE_LINES = get_max_lines()
 MAX_PAGE_WIDTH = get_max_columns()
 
+# Parser setup
+parser = argparse.ArgumentParser()
+parser.add_argument("command", help="The command to run", nargs="?")
+parser.add_argument("subcommand", help="Optional sub-command.", nargs="?")
+parser.add_argument("-l", "--list", help="List the commands available",
+    action="store_true")
+args = parser.parse_args()
+
 # Command Methods
 def command_init():
     """Foo"""
     pass
 
+def command_help():
+    """Display the help system"""
+    if args.subcommand in commands:
+        pass
+    else:
+        parser.print_usage()
+
 commands = {
         'init' : {
             'method' : command_init,
-            'desc' : 'Initialize the system.'
+            'desc' : 'Initialize the system.',
+            'help' : [
+                'Sub-commands:',
+                '-------------',
+                '',
+                'all : Initialize all sub-systems. This is the ' + \
+                        'default behavior if no sub-command is specified',
+                'config : Initialize/prepare a default configuration',
+                'bank : Initialize the bank'
+                ]
+        },
+        'help' : {
+            'method' : command_help,
+            'desc' : 'Display vamp command help.',
+            'help' : [
+                'When called with no sub-command, will display the ' + \
+                'vamp usage information.',
+                '',
+                'When called with the name of a command, display the help ' + \
+                'for that command.'
+                ]
         }
     }
 
 # Main entry point
 def run():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("command", help="The command to run", nargs="?")
-    parser.add_argument("-l", "--list", help="List the commands available",
-        action="store_true")
-    args = parser.parse_args()
-
     if args.list:
         key_len = len(max(commands.keys(), key=len))
         desc_len = MAX_PAGE_WIDTH - 5 - key_len
