db5da7958e93368caef9d85a5246fbd3a177af8c
Sam Hart <criswellious@gmail.com>
2016-02-24 11:41:17 -0500

    start work on base package class

--- vamp/package.py
+++ vamp/package.py
@@ -3,6 +3,28 @@ import sys
 from vamp.config import Config
 from vamp.bank import Bank
 
+class PackageBase:
+    """The base definition of a package class."""
+
+    # Simple requirements array. Should contain a list of packages this
+    # package requires
+    requires = []
+
+    def __init__(self):
+        pass
+
+    def check_system_requirements(self):
+        """Check the system requirements.
+
+        When called, this should check to see if the necessary things are
+        present on the system for this package to be installed and run.
+
+        Returns True if they are met or False if they are not."""
+        pass
+
+    def test(self):
+        print("Test from base class!")
+
 class PackageHandler:
     __borg_state = {}
 
